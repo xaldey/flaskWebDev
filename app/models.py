1c327c6cc80fb5f009f8d321932057691ce153a9
xaldey <xaldey@mail.ru>
2020-05-31 02:38:35 +0300

    Не работает ссылка сайта для авторизации. Надо разобраться. Создание пользователей пока через "flask shell"

--- app/models.py
+++ app/models.py
@@ -1,8 +1,14 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from . import db
+from . import db, login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 # Делаю класс Роли для пользователя
@@ -16,10 +22,12 @@ class Role(db.Model):
         return '<Роль %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     password_hash = db.Column(db.String(128))
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,7 @@
 import unittest
 from app.models import User
 
+
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
         u = User(password='cat')

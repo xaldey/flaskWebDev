eda3ec0cbaa3d002b613b3d0b746677a46c5c20a
xaldey <xaldey@mail.ru>
2020-05-31 01:21:39 +0300

    Рефакторинг завершен. Проблема с БД: та самая posts_tags таблица

---
+++ app/models.py
@@ -0,0 +1,71 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from . import db
+
+
+# Делаю класс Роли для пользователя
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Роль %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('пароль - не читаемый атрибут')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<Пользователь %r>' % self.username
+
+#
+# class Post(db.Model):
+#     __table__ = 'posts'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     title = db.Column(db.String(64), nullable=False)
+#     text = db.Column(db.Text, nullable=False)
+#     is_published = db.Column(db.Boolean, nullable=False, default=False)
+#
+#     user = relationship(User, back_populates='posts')
+#     tags = relationship('Tag', secondary=posts_tags_table, back_populates='posts')
+#
+#     def __repr__(self):
+#         return f'<Пост #{self.id} от {self.user} {self.title}.'
+#
+#
+# class Tag(db.Model):
+#     __tablename__ = 'tags'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(20), nullable=False)
+#
+#     posts = relationship('Post', secondary=posts_tags_table, back_populates='tags')
+#
+#     def __repr__(self):
+#         return f'<Тэг #{self.id} {self.name}>'
+
+
+class NameForm(FlaskForm):
+    name = StringField('Как к тебе обращаться?', validators=[DataRequired()])
+    submit = SubmitField('Отправить')
--- myblog.py
+++ myblog.py
@@ -1,23 +1,23 @@
-from sqlalchemy.orm import relationship
-
-from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
+import os
+from app import create_app, db
+from app.models import User, Role
 from flask_migrate import Migrate
-from flask_login import UserMixin
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 #
 # posts_tags_table = db.Table(
@@ -32,88 +32,3 @@ migrate = Migrate(app, db)
 # 	"tag_id"	INTEGER,
 # 	PRIMARY KEY("post_id","tag_id")
 # );
-
-
-
-# Делаю класс Роли для пользователя
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<Роль %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-
-    def __repr__(self):
-        return '<Пользователь %r>' % self.username
-
-#
-# class Post(db.Model):
-#     __table__ = 'posts'
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     title = db.Column(db.String(64), nullable=False)
-#     text = db.Column(db.Text, nullable=False)
-#     is_published = db.Column(db.Boolean, nullable=False, default=False)
-#
-#     user = relationship(User, back_populates='posts')
-#     tags = relationship('Tag', secondary=posts_tags_table, back_populates='posts')
-#
-#     def __repr__(self):
-#         return f'<Пост #{self.id} от {self.user} {self.title}.'
-#
-#
-# class Tag(db.Model):
-#     __tablename__ = 'tags'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(20), nullable=False)
-#
-#     posts = relationship('Post', secondary=posts_tags_table, back_populates='tags')
-#
-#     def __repr__(self):
-#         return f'<Тэг #{self.id} {self.name}>'
-
-
-class NameForm(FlaskForm):
-    name = StringField('Как к тебе обращаться?', validators=[DataRequired()])
-    submit = SubmitField('Отправить')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))

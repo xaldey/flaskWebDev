dc91c51bfcbf1db83f8c8e23110199cfca056f80
xaldey <xaldey@mail.ru>
2020-06-07 09:59:29 +0300

    Обновил файл требований

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
+from ..email import send_email
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 
@@ -15,8 +16,11 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Теперь вы можете войти')
-        return redirect(url_for('auth.login'))
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Подтвердить ваш аккаунт',
+                   'auth/email/confirm', user=user, token=token)
+        flash('Подтверждение направлено на вашу почту')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
 
 
@@ -40,4 +44,33 @@ def login():
 def logout():
     logout_user()
     flash('Вы успешно разлогинились')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+        and not current_user.confirmed \
+        and request.blueprint != 'auth' \
+        and request.endpoint != 'static':
+    return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main/index'))
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('Ваша учетная запись подтверждена. Спасибо!')
+    else:
+        flash('Ссылка для подтверждения истекла, либо неисправна.')
     return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -29,6 +31,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
 
     password_hash = db.Column(db.String(128))
 
@@ -42,6 +45,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<Пользователь %r>' % self.username
